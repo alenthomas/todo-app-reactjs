f2047d999cfaf32ba98751cc4a77e9b4024e021c
Alen Thomas <alenthomas9@gmail.com>
2017-11-05 15:43:08 +0530

    Import images with require
           - Modify image extension in data.json
           - Images.js imports these files as a js object

---
+++ src/images.js
@@ -0,0 +1,49 @@
+import ad from './assets/images/ad.jpg'
+import bh from './assets/images/bh.jpg'
+import bl from './assets/images/bl.jpg'
+import cdc from './assets/images/cdc.jpg'
+import dd from './assets/images/dd.jpg'
+import fh from './assets/images/fh.jpg'
+import f from './assets/images/f.jpg'
+import hg from './assets/images/hg.jpg'
+import hoc from './assets/images/hoc.jpg'
+import jj from './assets/images/jj.jpg'
+import l from './assets/images/l.jpg'
+import loading from './assets/images/loading.png'
+import mam from './assets/images/mam.jpg'
+import mon from './assets/images/mon.jpg'
+import mp from './assets/images/mp.jpg'
+import n from './assets/images/n.jpg'
+import oitnb from './assets/images/oitnb.jpg'
+import s8 from './assets/images/s8.jpg'
+import tk from './assets/images/tk.jpg'
+import uks from './assets/images/uks.jpg'
+import wbd from './assets/images/wbd.jpg'
+import whas from './assets/images/whas.jpg'
+
+const imgs = {
+  ad: ad,
+  bh: bh,
+  bl: bl,
+  cdc: cdc,
+  dd: dd,
+  fh: fh,
+  f: f,
+  hg: hg,
+  hoc: hoc,
+  jj: jj,
+  l: l,
+  loading: loading,
+  mam: mam,
+  mon: mon,
+  mp: mp,
+  n: n,
+  oitnb: oitnb,
+  s8: s8,
+  tk: tk,
+  uks: uks,
+  wbd: wbd,
+  whas: whas
+}
+
+export default imgs
