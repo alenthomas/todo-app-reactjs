a34cd045da22bdcaebc215a2959237fe4b06dff9
Alen Thomas <alenthomas9@gmail.com>
2017-11-05 15:45:41 +0530

    Add card components to search page
        - Display data.json in card
        - Display image, title, description and year

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react'
 
 import preload from './assets/data.json'
+import imgs from './images'
+
+const showTitle = (show) => {
+  return (
+    <div className='show-card'>
+      <img src={imgs[show.poster]} alt={show.poster} />
+      <div>
+        <h3>{show.title}</h3>
+        <h4>({show.year})</h4>
+        <p>{show.description}</p>
+      </div>
+    </div>
+  )
+}
 
 class Search extends Component {
   render () {
     return (
       <div className='search'>
-        <pre><code>{JSON.stringify(preload, null, 2)}</code></pre>
+        {/* <pre><code>{JSON.stringify(preload, null, 2)}</code></pre> */}
+        {preload.shows.map(showTitle)}
       </div>
     )
   }
